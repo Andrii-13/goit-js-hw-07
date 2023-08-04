264791308fe54b3379139469e325f1eea8c9358b
Andrii <gowar@ukr.net>
2023-08-04 21:17:14 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+getGallery(galleryItems);
+
+gallery.addEventListener("click", handlerOpenModal);
+
+function getGallery(arr) {
+  const itemLi = addMarkup(arr);
+  addListItem(itemLi);
+}
+
+function addMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+      <a href="${preview}" class="gallery__link" onclick="event.preventDefault()">
+        <img src="${preview}" alt="${description}" class="gallery__image" data-source="${original}" />
+      </a>
+    </li>`
+    )
+    .join("");
+}
+
+function addListItem(item) {
+  gallery.insertAdjacentHTML("beforeend", item);
+}
+
+function handlerOpenModal(e) {
+  if (e.target.classList.value !== "gallery__image") {
+    return;
+  }
+
+  const modal = basicLightbox.create(`
+<div>
+  <img src="${e.target.dataset.source}" />
+</div>
+`);
+  modal.show();
+}
+
+onClose: (instance) => {
+  console.log(object);
+};
+// console.log(galleryItems);
